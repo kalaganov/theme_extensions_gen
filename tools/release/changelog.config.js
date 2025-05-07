b2c32917750403b374f49297440917db7868cd2c
Eugene <13645580+kalaganov@users.noreply.github.com>
2025-05-07 21:33:28 +0700

    chore: test chore type

--- tools/release/changelog.config.js
+++ tools/release/changelog.config.js
@@ -1,7 +1,5 @@
+console.log('changelog.config.js loaded');
 module.exports = {
-  skip: { tag: false },
-  header: '',
-  releaseCommitMessageFormat: 'chore(release): {{currentTag}}',
   types: [
     { type: 'feat', section: 'Features' },
     { type: 'fix', section: 'Bug Fixes' },
@@ -12,13 +10,17 @@ module.exports = {
     { type: 'style', section: 'Styles' },
     { type: 'test', section: 'Tests' }
   ],
+  bumpFiles: [],
+  skip: { tag: false },
+  packageFiles: [],
   conventionalChangelog: {
+    preset: 'conventionalcommits',
     linkCompare: false,
     linkReferences: false,
     writerOpts: {
       headerPartial:
         'Changelog for v{{version}}\\n' +
-        '---------------------\\n\\n',
+        '---------------------------\\n\\n',
       commitPartial:
         '* {{type}}: {{#if scope}}`{{scope}}` - {{/if}}{{subject}}' +
         '{{#if references}} ({{#each references}}#{{this.issue}}' +
