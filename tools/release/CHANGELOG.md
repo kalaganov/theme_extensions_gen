b26e809f3d6966eee1e78b2c79e80413f273321b
Eugene <13645580+kalaganov@users.noreply.github.com>
2025-05-07 21:27:16 +0700

    chore: test chore type

--- tools/release/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Changelog
-
-All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
-
-### [1.0.14](https://github.com/kalaganov/theme_extensions_gen/compare/v0.1.0...v1.0.14) (2025-05-07)
--- tools/release/changelog.config.js
+++ tools/release/changelog.config.js
@@ -1,13 +1,7 @@
-// tools/release/changelog.config.js
 module.exports = {
-  infile: '../../packages/theme_extensions_gen/CHANGELOG.md',
-
-  commitUrlFormat: '',
-  compareUrlFormat: '',
-  linkCompare: false,
-  linkReferences: false,
-  headerPartial: '+++++++++++++'
-  context: { linkCompare: false, linkReferences: false },
+  skip: { tag: false },
+  header: '',
+  releaseCommitMessageFormat: 'chore(release): {{currentTag}}',
   types: [
     { type: 'feat', section: 'Features' },
     { type: 'fix', section: 'Bug Fixes' },
@@ -18,12 +12,17 @@ module.exports = {
     { type: 'style', section: 'Styles' },
     { type: 'test', section: 'Tests' }
   ],
-  writerOpts: {
-    headerPartial: 'Changelog for v{{version}}\\n' +
-                   '---------------------\\n\\n',
-    commitPartial:
-      '* {{type}}: {{#if scope}}`{{scope}}` - {{/if}}{{subject}}' +
-      '{{#if references}} ({{#each references}}#{{this.issue}}' +
-      '{{#unless @last}}, {{/unless}}{{/each}}){{/if}}\\n'
+  conventionalChangelog: {
+    linkCompare: false,
+    linkReferences: false,
+    writerOpts: {
+      headerPartial:
+        'Changelog for v{{version}}\\n' +
+        '---------------------\\n\\n',
+      commitPartial:
+        '* {{type}}: {{#if scope}}`{{scope}}` - {{/if}}{{subject}}' +
+        '{{#if references}} ({{#each references}}#{{this.issue}}' +
+        '{{#unless @last}}, {{/unless}}{{/each}}){{/if}}\\n'
+    }
   }
 };
